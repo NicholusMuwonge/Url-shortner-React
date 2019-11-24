37dc0be541780d4f9646b6b3b093eb2a80da72b9
NicholusMuwonge <nicholusmuwonge@gmail.com>
2019-11-24 15:29:31 +0300

    fix indentation on small screens

--- src/UderProduction.js
+++ src/UderProduction.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const UnderProduction = () =>(
 
-        <h5>Page Under Production</h5>
+        <h5  className='text-center display-2' style={{marginTop:'20rem'}}>Page Under Production</h5>
 )
 
 export default UnderProduction;
